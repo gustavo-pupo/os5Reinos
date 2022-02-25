0ce6cfed832d7276afcf0132d33acdadef188a82
gustavo-pupo <gusfepupo@gmail.com>
2022-02-25 11:56:53 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'reinosAng';
 
-  
+    contador:number = 1;
+    incomoda:string = 'incomodam ';
+
+    elefanteFdp(){
+        while(this.contador<=50){
+            if(this.contador == 1){
+                alert(`${this.contador} elefante incomoda muita gente.`);
+                this.contador++;
+            }else{
+            alert(this.contador + ' elefantes ' + this.incomoda.repeat(this.contador) +'muito mais.');
+            this.contador++;
+            alert(`${this.contador} elefantes incomodam muita gente.`);
+            this.contador++;
+            }
+        }
+        alert('Parabéns pela paciência');
+        alert('Muito obrigado');
+    }
 }
